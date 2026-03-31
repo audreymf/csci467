a94bd692e7c7c48b04e366bc722ea9f05b50cc97
audreymf <a1989163@mail.niu.edu>
2026-03-31 17:36:24 -0500

    Create customer.js

---
+++ customer.js
@@ -0,0 +1,19 @@
+var mysql = require('mysql')
+var connection = mysql.createConnection({
+  host: 'blitz.cs.niu.edu',
+  user: 'student',
+  password: 'student',
+  database: 'csci467'
+});
+
+connection.connect();
+
+module.exports = {
+    getAll: async result => {
+        connection.query('SELECT * FROM customers', function(err, rows){
+            if (err) throw err;
+            console.log('found ', rows.length, ' customers');
+            result(rows);
+        });
+    }
+}

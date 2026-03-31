2965930f7844c041324c9b22255a850efbd5b8b5
audreymf <a1989163@mail.niu.edu>
2026-03-31 17:55:53 -0500

    Create quotes.js

---
+++ controllers/quotes.js
@@ -0,0 +1,7 @@
+/*
+  Backend (Role 2), Internal Systems Eng (Role 4)
+  Main functionality of the system:
+  - creates/edits quotes from draft to order placement (and the steps in between)
+  - line items/note management
+  - calculate pricing + discounts
+*/

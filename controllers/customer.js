ef2fd5f8ceb6c4de60930dfc41a3f711c399a3bb
audreymf <a1989163@mail.niu.edu>
2026-04-07 20:35:53 -0500

    Update customer.js
    Added connection to our DB in XAMPP and module exports

--- controllers/customer.js
+++ controllers/customer.js
@@ -12,13 +12,75 @@ var connection = mysql.createConnection({
   database: 'csci467'
 });
 
-connection.connect();
+// Connection to our created DB
+var system_db = mysql.createConnection({
+  host: '10.168.191.95',
+  port: 3306,
+  user: 'team',
+  password: 'ege467',
+  database: 'quote_system'
+});
 
+// Connection to our created DB
+// I did not open this to public networks so it should work
+// with the specific login info included here
+var system_db = mysql.createConnection({
+  host: '10.168.191.95',
+  port: 3306,
+  user: 'team',
+  password: 'ege467',
+  database: 'quote_system'
+});
+
+// Throw error if we cannot connect to legacy DB
+legacy_db.connect(err => {
+  if(err) throw errl
+  console.log('Connected to legacy database');
+}
+                  
+// sane for our DB
+system_db.connect(err => {
+  if(err) throw errl
+  console.log('Connected to quote_system database');
+}   
+
+// Not sure if we need this here anymore but I added exports for our own tables too
 module.exports = {
-    getAll: async result => {
+    getLegacyCustomers: async result => {
         connection.query('SELECT * FROM customers', function(err, rows){
             if (err) throw err;
             console.log('found ', rows.length, ' customers');
+            result(rows);
+        });
+    }
+  getSalesAssociates: async result => {
+        connection.query('SELECT * FROM sales_associates', function(err, rows){
+            if (err) throw err;
+            console.log('found ', rows.length, ' sales_associates');
+            result(rows);
+        });
+    }
+
+  getQuotes: async result => {
+        connection.query('SELECT * FROM quotes', function(err, rows){
+            if (err) throw err;
+            console.log('found ', rows.length, ' quotes');
+            result(rows);
+        });
+    }
+
+  getLineItems: async result => {
+        connection.query('SELECT * FROM line_items', function(err, rows){
+            if (err) throw err;
+            console.log('found ', rows.length, ' line_items');
+            result(rows);
+        });
+    }
+
+  getNotes: async result => {
+        connection.query('SELECT * FROM notes', function(err, rows){
+            if (err) throw err;
+            console.log('found ', rows.length, ' notes');
             result(rows);
         });
     }

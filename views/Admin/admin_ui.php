aba6f935e4fb5a78c092185175786d9b63f772ca
javoncherry2 <javoncherry2@gmail.com>
2026-04-19 02:29:51 -0500

    Updated Admin_ui.php

--- views/Admin/admin_ui.php
+++ views/Admin/admin_ui.php
@@ -40,7 +40,7 @@ if(isset($_GET['edit']) || isset($_GET['leave'])){
 
 #if add sales associate is chosen
 if(isset($_GET['new_associate'])){
-	add($pdoq, $_GET['newname'], $_GET['newpassword'], $_GET['newuserid']);
+	add($pdoq, $_GET['newname'], $_GET['newpassword'], $_GET['newuserid'], $_GET['position']);
 			         }
 
 
@@ -116,13 +116,26 @@ echo "</tr>";
 echo "<tr>";
 echo "<td class=newsalabel>New UserID:</td>";
 echo "<td><input class=textbox type='text' name='newuserid' required></td>";
-echo "<tr>";
+echo "</tr>";
 
 #Input for new password
 echo "<tr>";
 echo "<td class=newsalabel>New Password:</td>";
 echo "<td><input class=textbox type='text' name='newpassword' required></td>";
+echo "</tr>";
+
+#Input for new Position
 echo "<tr>";
+echo "<td class=newsalabel>Position:</td>";
+echo "<td class=textbox>";
+echo "<select name='position'>";
+echo "<option value='sales'>Sales</option>";
+echo "<option value='hq'>HQ</option>";
+echo "<option value='admin'>Admin</option>";
+echo "</select>";
+echo "</td>";
+echo "</tr>";
+
 echo "</table>";
 
 #Submit and Clear button for adding new sales associate
@@ -136,29 +149,148 @@ echo "</div>";
 
 #section for Quotes
 else{
-	echo "<h1 class='header' style='margin-top: 60px; padding-left: 40px;'>Company Quotes</h1>";
+	echo "<br>";
+	echo "<form method=get>";
 
-#
-	$sql = "SELECT  DATE(date), customerID,  sales_associates.name, quotes.commission, status
- 		FROM quotes 
-		JOIN sales_associates ON associateID = sales_associates.id;";
+#Form for date
+	echo "Start date: ";
+	if(isset($_GET['startdate'])){
+    		echo "<input type='date' name='startdate' value='" . $_GET['startdate'] . "' />";
+				     } 
+	else{
+    		echo "<input type='date' name='startdate' value='" . date('Y-m-d') . "' />";
+	    }
+
+	echo " to: "; 
+
+	if(isset($_GET['enddate'])){
+ 		echo "<input type='date' name='enddate' value='" . $_GET['enddate'] . "' />";
+				   } 
+	else {
+    echo "<input type='date' name='enddate' value='" . date('Y-m-d') . "' />";
+	     }
+
+	echo "<br>";
+	echo "<br>";
+
+#Form for SA
+	echo "Sales associate: ";
+	echo "<select name=SalesAssociate>";
+
+	$sql = "SELECT DISTINCT name
+		FROM quotes
+		JOIN sales_associates ON sales_associates.id = quotes.associateID;";
+
 	$stmt = $pdoq->query($sql);
 	$rows = $stmt->fetchAll();
 
-	echo "<div class=sa-table>";
-	echo "<table class=table-data>";
+#if All should be selected- Sales Associate
+	if($_GET['SalesAssociate'] == 'All'){
+   		$selectedAll = "selected";
+					    }
+       	else {
+        	$selectedAll = "";
+  	     }	
+
+	echo "<option value='All' $selectedAll>All</option>";
+
+#if Sa should be selected- Sales Associates
 
 	foreach($rows as $row){
-		echo "<tr>";
-		echo "<td class=id_name>$row[0]</td>";
-		echo "<td class=id_name>$row[1]</td>";
-		echo "<td class=id_name>$row[2]</td>";
-		echo "<td class=id_name>$row[3]</td>";
-		echo "<td class=id_name>$row[4]</td>";
-		echo "</tr>";
+
+		if($_GET['SalesAssociate'] == $row[0]){
+  		  $saselected = "selected";
+						      } 
+		else {
+    		  $saselected = "";
+		     }
+		echo "<option value='$row[0]' $saselected>$row[0]</option>";
 			      }
-	echo "</table>";
-	echo "</div>";	
+	echo "</select>";
+
+#Form for Customer
+	echo "  Customer: ";
+	echo "<select name=Customer>";
+
+#if All should be selected -Customer
+if(!isset($_GET['Customer']) || $_GET['Customer'] == 'All'){
+	$selectedAll = "selected";
+							   }
+else {
+    $selectedAll = "";
+     }
+echo "<option value='All' $selectedAll>All</option>";
+
+
+#Gets customer id that are in quotes
+	$sql = "SELECT DISTINCT customerID FROM quotes";
+	$stmt = $pdoq->query($sql);
+	$ids = $stmt->fetchAll();
+
+#uses id to get names from customer db
+foreach($ids as $id){
+	$sql = "SELECT name 
+		FROM customers
+		WHERE id = ?";
+
+	$stmt = $pdoc->prepare($sql);
+	$stmt->execute([$id[0]]);
+	$row = $stmt->fetch();
+
+#if Customer should be selected - Customer
+	if($_GET['Customer'] == $row[0]){
+        $selectedAll = "selected";
+					}
+       	else {
+        $selectedAll = "";
+   	     }
+
+        echo "<option value='$row[0]' $selectedAll>$row[0]</option>";
+		    }
+	echo "</select>";
+
+#Form for Status
+	echo " Status: ";
+	echo "<select name=Status>";
+
+#If all should be selected -Status
+	if($_GET['Status'] == 'All'){
+		$AllStatus = "selected";
+				    }
+	else{
+		$AllStatus = "";
+	    }
+	echo "<option value=All $AllStatus>All</option>";
+
+#A status should be selected   -Status
+	
+	$statuses = ['Finalized', 'Sanctioned', 'Ordered'];
+
+	foreach($statuses as $status){
+        	if($_GET['Status'] == $status){
+        		$statusselected = "selected";
+					      } 
+		else {
+        		$statusselected = "";
+  		     }
+		echo "<option value='$status' $statusselected>$status</option>";
+				     }
+	echo "</select>";
+
+
+#Submit Form
+	echo "<br>";	
+	echo "<input type=hidden value=Quotes name=view>";
+	echo "<input type=submit value=Sort name=sort>";
+	
+
+	echo "</form>";
+
+	echo "<h1 class='header' style='margin-top: 30px; padding-left: 40px; '>Company Quotes</h1>";
+
+#Show table
+	table($_GET['startdate'],$_GET['enddate'],$_GET['SalesAssociate'],$_GET['Customer'], $_GET['Status'], $pdoc, $pdoq);
+
     }
 ?>
 </body>

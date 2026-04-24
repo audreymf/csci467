c60eb1141b25c0491cd163952c1119bbf1c0b497
javoncherry2 <javoncherry2@gmail.com>
2026-04-23 22:31:43 -0500

    Refactor Functions.php for code consistency

--- views/Admin/Functions.php
+++ views/Admin/Functions.php
@@ -14,7 +14,7 @@ function remove($pdoq, $id){
         $sql = "DELETE FROM sales_associates WHERE id = ?";
         $statement = $pdoq->prepare($sql);
         $statement->execute([$id]);
-			  }
+			 			  }
 
 /*
  *This functions edits the selected sales associate and updated the value in the database
@@ -29,7 +29,7 @@ function edit($pdoq, $id){
 	#if cancel was chosen
         if($_GET['leave'] == 'Cancel'){
             return;
-				      }
+				      				  }
 
 	#if submit was chosen
 	if($_GET['leave'] == 'Submit'){
@@ -47,53 +47,53 @@ function edit($pdoq, $id){
         $statement->execute([$id]);
 	$row = $statement->fetch();
 
-//makes a pop up to enter new info
-echo "<dialog open class=modalstyle>";
-echo "<h1 class=formheader>Editing Associate</h1>";
+#makes a pop up to enter new info
+	echo "<dialog open class=modalstyle>";
+	echo "<h1 class=formheader>Editing Associate</h1>";
 
 #edit form info
-echo "<form method=GET>";
-echo "<input type='hidden' name='id' value='$id'>";
+	echo "<form method=GET>";
+	echo "<input type='hidden' name='id' value='$id'>";
 
-echo "<table>";
+	echo "<table>";
 
 #edit name section
-echo "<tr>";
-echo "<td class=modallabel>Name:</td>";
-echo "<td><input class=textbox type='text' name='name' value='$row[0]' required></td>";
-echo "</tr>";
+	echo "<tr>";
+	echo "<td class=modallabel>Name:</td>";
+	echo "<td><input class=textbox type='text' name='name' value='$row[0]' required></td>";
+	echo "</tr>";
 
 #edit address section
-echo "<tr>";
-echo "<td class=modallabel>Address:</td>";
-echo "<td><input class=textbox type='text' name='address' value='$row[1]' required></td>";
-echo "</tr>";
+	echo "<tr>";
+	echo "<td class=modallabel>Address:</td>";
+	echo "<td><input class=textbox type='text' name='address' value='$row[1]' required></td>";
+	echo "</tr>";
 
 #edit UserID section
-echo "<tr>";
-echo "<td class=modallabel>UserID:</td>";
-echo "<td><input class=textbox type='text' name='UserID' value='$row[2]' required></td>";
-echo "</tr>";
+	echo "<tr>";
+	echo "<td class=modallabel>UserID:</td>";
+	echo "<td><input class=textbox type='text' name='UserID' value='$row[2]' required></td>";
+	echo "</tr>";
 
 #edit password section
-echo "<tr>";
-echo "<td class=modallabel>Password:</td>";
-echo "<td><input class=textbox type='text' name='password' value='$row[3]' required></td>";
-echo "</tr>";
-
+	echo "<tr>";
+	echo "<td class=modallabel>Password:</td>";
+	echo "<td><input class=textbox type='text' name='password' value='$row[3]' required></td>";
+	echo "</tr>";
+	
 #edit commission section
-echo "<tr>";
-echo "<td class=modallabel>Commission:</td>";
-echo "<td><input class=textbox type='number' name='commission' value='$row[4]' step=0.01 required></td>";
-echo "</tr>";
+	echo "<tr>";
+	echo "<td class=modallabel>Commission:</td>";
+	echo "<td><input class=textbox type='number' name='commission' value='$row[4]' step=0.01 required></td>";
+	echo "</tr>";
 
-echo "</table>";
+	echo "</table>";
 
 #submit or clear section
-echo "<input class=clear type='submit' name='leave' value='Cancel'/>";
-echo "<input class=submit type='submit' name='leave' value='Submit'/>";
-echo "</form>";
-echo "</dialog>";
+	echo "<input class=clear type='submit' name='leave' value='Cancel'/>";
+	echo "<input class=submit type='submit' name='leave' value='Submit'/>";
+	echo "</form>";
+	echo "</dialog>";
                         }
 
 /*
@@ -104,7 +104,7 @@ function add($pdoq, $new_name, $new_password, $new_userid, $position){
 	$sql = "INSERT INTO sales_associates(name, userID, password, access)  values(?, ?, ?, ?)";
 	$stmt = $pdoq->prepare($sql);
 	$stmt->execute([$new_name, $new_password, $new_userid, $position]);
-					                  	     }
+					                  	     						}
 
 /*
  * This functions sorts and creates the table of quotes
@@ -114,7 +114,7 @@ function table($sdate, $edate, $sa, $customer, $status, $pdoc, $pdoq){
 	if($edate < $sdate){
 		echo "<p class=error>***Ending date must be after starting date***</p>";
 		die();
-			   }
+			  		  }
 #gets customer info
 	$sql = "SELECT id FROM customers WHERE name = ?";
 	$stmt = $pdoc->prepare($sql);
@@ -123,7 +123,7 @@ function table($sdate, $edate, $sa, $customer, $status, $pdoc, $pdoq){
 
 
 #Starting sql for sorted table
-	$sql = "SELECT DATE(date), customerID,  sales_associates.name, quotes.commission, status, quotes.id
+	$sql = "SELECT DATE(date), customerID,  sales_associates.name, COALESCE(quotes.commission,0), status, quotes.id
 		FROM quotes 
 		JOIN sales_associates ON associateID  = sales_associates.id
 		WHERE date BETWEEN ? AND ?";	
@@ -135,19 +135,19 @@ function table($sdate, $edate, $sa, $customer, $status, $pdoc, $pdoq){
 	if($sa != 'All'){
 		$sql .= " AND sales_associates.name = ?";
 		$params[] = $sa;
-			}
+					}
 
 #if customer was chosen
 	if($customer != 'All'){
 		$sql .= " AND customerID = ?";
 		$params[] = $cid[0];
-			      }
+			 		     }
 
 #if status was chosen
 	if($status != 'All'){
 		$sql .= " And status = ?";
 		$params[] = $status;
-			    }
+			   			 }
 
 	$stmt = $pdoq->prepare($sql);
 	$stmt->execute($params);
@@ -157,88 +157,88 @@ function table($sdate, $edate, $sa, $customer, $status, $pdoc, $pdoq){
 #if no rows selected
 if(count($rows) == 0){
 	echo "<p class='error'>***No quotes available***</p>";
-		     }	
+		     		}	
 
 else{
 #Show table
 
-echo "<div class='sa-table'>";
-echo "<table class='table-data'>";
+	echo "<div class='sa-table'>";
+	echo "<table class='table-data'>";
 
 
 #Headers for sales associate table
-echo "<tr>";
-echo "<th class=table-headers>Date</th>";
-echo "<th class=table-headers>Customer</th>";
-echo "<th class=table-headers>Sales Associate</th>";
-echo "<th class=table-headers>Commission</th>";
-echo "<th class=table-headers style='padding-left: 50px;'>Status</th>";
-echo "<th class=table-headers></th>";
-echo "<tr>";
+	echo "<tr>";
+	echo "<th class=table-headers>Date</th>";
+	echo "<th class=table-headers>Customer</th>";
+	echo "<th class=table-headers>Sales Associate</th>";
+	echo "<th class=table-headers>Commission</th>";
+	echo "<th class=table-headers style='padding-left: 50px;'>Status</th>";
+	echo "<th class=table-headers></th>";
+	echo "<tr>";
 
-foreach($rows as $row){
+	foreach($rows as $row){
 //get customer info from customers db
-    $sql = "SELECT name, city, street, contact FROM customers WHERE id = ?";
-    $stmt = $pdoc->prepare($sql);
-    $stmt->execute([$row[1]]);
-    $crow = $stmt->fetch();
-    $cname = $crow[0];
-    $ccity = $crow[1];
-    $cstreet = $crow[2];
-    $ccontact = $crow[3];
+  		$sql = "SELECT name, city, street, contact FROM customers WHERE id = ?";
+    		$stmt = $pdoc->prepare($sql);
+   		$stmt->execute([$row[1]]);
+    		$crow = $stmt->fetch();
+    		$cname = $crow[0];
+    		$ccity = $crow[1];
+    		$cstreet = $crow[2];
+    		$ccontact = $crow[3];
 
-    echo "<tr>";
-    echo "<td class='id_name'>$row[0]</td>";
-    echo "<td class='id_name'>$cname</td>"; 
-    echo "<td class='id_name'>$row[2]</td>";
-    echo "<td class='commission'>$$row[3]</td>"; 
-    echo "<td style='padding-left: 50px;'>$row[4]</td>";
+    	echo "<tr>";
+    	echo "<td class='id_name'>$row[0]</td>";
+    	echo "<td class='id_name'>$cname</td>"; 
+   		echo "<td class='id_name'>$row[2]</td>";
+   		echo "<td class='commission'>$$row[3]</td>"; 
+   		echo "<td style='padding-left: 50px;'>$row[4]</td>";
 
 #get line items associates with the quote
-$sql = "SELECT item, price FROM line_items WHERE quoteID = ?";
-$stmt = $pdoq->prepare($sql);
-$stmt->execute([$row[5]]);
-$items = $stmt->fetchAll();
+		$sql = "SELECT item, price FROM line_items WHERE quoteID = ?";
+		$stmt = $pdoq->prepare($sql);
+		$stmt->execute([$row[5]]);
+		$items = $stmt->fetchAll();
 
 
 #stores all items
-$itemList = [];
-foreach($items as $item){
-    $itemList[] = ["item" => $item[0], "price" => $item[1]];
-			}
-$itemJson = json_encode($itemList);
+		$itemList = [];
+		foreach($items as $item){
+   			 $itemList[] = ["item" => $item[0], "price" => $item[1]];
+								}
+		$itemJson = json_encode($itemList);
 
 #get notes
-$sql = "SELECT content FROM notes WHERE quoteID = ?";
-$stmt = $pdoq->prepare($sql);
-$stmt->execute([$row[5]]);
-$notes = $stmt->fetchAll();
+		$sql = "SELECT content FROM notes WHERE quoteID = ?";
+		$stmt = $pdoq->prepare($sql);
+		$stmt->execute([$row[5]]);
+		$notes = $stmt->fetchAll();
 
 #stores all notes
-$notesList = [];
-foreach($notes as $note){
-    $notesList[] = $note[0];
-			}
+		$notesList = [];
+		foreach($notes as $note){
+    			$notesList[] = $note[0];
+								}
 
 #Converts both to Json String
-$itemJson = json_encode($itemList);
-$notesJson = json_encode($notesList);
+		$itemJson = json_encode($itemList);
+		$notesJson = json_encode($notesList);
 
 
 #Button For viewing quote
-echo "<td>";
-echo "<button onclick='openModal(\"{$row[0]}\", \"{$cname}\", \"{$ccity}\", \"{$cstreet}\", \"{$ccontact}\", \"{$row[3]}\", {$itemJson}, {$notesJson})' class='view'>View</button>";
-echo "</td>";
-echo "</tr>";
+		echo "<td>";
+		echo "<button onclick='openModal(\"{$row[0]}\", \"{$cname}\", \"{$ccity}\", \"{$cstreet}\", \"{$ccontact}\", \"{$row[3]}\", {$itemJson}, {$notesJson})' class='view'>View</button>";
+		echo "</td>";
+		echo "</tr>";
 		     }
-echo "</table>";
-echo "</div>";							    }
-	}
+		echo "</table>";
+		echo "</div>";							    }
+			}
 
 ?>
 
 <!-- 
-Functions To create Popup for viewing quotes-->
+Functions To Display the details of the quote-->
 <div id="myModal" style="display:none; position:fixed; top:0; left:0; width:100%; height:100%; background:rgba(0,0,0,0.5); z-index:9999; justify-content:center; align-items:center;">
 <div style="background:#fff; padding:30px; border-radius:8px; min-width:400px;">
 <h1>Viewing Quote Details</h1>
@@ -260,29 +260,30 @@ function openModal(date, cname, ccity, cstreet, ccontact, commission, lineitems,
 //if not line items
 	if(lineitems.length == 0){
    		 itemRows = "<p>None</p>";
-				 }
+				 			}
 	else{
 	let count = 1;
 
-    	lineitems.forEach(i => {
+    		lineitems.forEach(i => {
 		itemRows += `<tr><td>${count}. ${i.item}</td><td style="text-align:right;">$${i.price}</td></tr>`;
 		count++
-							   });
+								   });
             }
 
-//if not notes
 	let noteRows = "";
 
+//if no notes
 	if(notes.length == 0){
    		 noteRows = "<p>None</p>";
-			     		 }
+			   			  }
 	else{
-	let count = 1;
+		let count = 1;
 
-    	notes.forEach(i => {
-		noteRows += `<tr><td>${count}. ${i}</td></tr>`;
-		count++
-			   });
+//adds next line item
+    		notes.forEach(i => {
+			noteRows += `<tr><td>${count}. ${i}</td></tr>`;
+			count++
+			   	  				 });
             }
 	document.getElementById('modalContent').innerHTML = `
 
@@ -300,7 +301,7 @@ function openModal(date, cname, ccity, cstreet, ccontact, commission, lineitems,
 		<table style="width:100%">${noteRows}</table>
 
 							   `;
-    document.getElementById('myModal').style.display = 'flex';
+   	 document.getElementById('myModal').style.display = 'flex';
   											}
 /*This functions closes the modal
   Takes no arguments
@@ -308,6 +309,7 @@ function openModal(date, cname, ccity, cstreet, ccontact, commission, lineitems,
 
   function closeModal() {
     	document.getElementById('myModal').style.display = 'none';
-  						}
+  				     	}
 </script>
+
 

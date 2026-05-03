0843c45c6a16f80b94a0d382c5ff16725e9c2328
audreymf <a1989163@mail.niu.edu>
2026-05-03 13:37:24 -0500

    Delete controllers/quotes.js
    empty file

--- controllers/quotes.js
+++
@@ -1,7 +0,0 @@
-/*
-  Backend (Role 2), Internal Systems Eng (Role 4)
-  Main functionality of the system:
-  - creates/edits quotes from draft to order placement (and the steps in between)
-  - line items/note management
-  - calculate pricing + discounts
-*/
